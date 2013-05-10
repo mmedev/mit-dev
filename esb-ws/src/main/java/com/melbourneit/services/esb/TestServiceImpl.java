cdacc211f9ad8d3687a6245aaddf0c31cd303859
Harris <Harris@NWS-PC28>
2013-05-10 16:12:40 +0800

    updated

--- esb-ws/src/main/java/com/melbourneit/services/esb/TestServiceImpl.java
+++ esb-ws/src/main/java/com/melbourneit/services/esb/TestServiceImpl.java
@@ -4,6 +4,7 @@ import org.apache.camel.ProducerTemplate;
 
 import com.melbourneit.exception.BaseException;
 import com.melbourneit.exception.JMSResquestException;
+import com.melbourneit.model.jms.JMSReqRes;
 import com.melbourneit.model.jms.domain.CheckDomainRequest;
 import com.melbourneit.utils.jms.JMSEndpoint;
 import com.melbourneit.utils.jms.JMSRequestProcessor;
@@ -13,7 +14,7 @@ public class TestServiceImpl extends GenericService implements TestService {
 	@Override
 	public String checkDomainName(String domainName) throws BaseException{
 		try{
-			return new JMSRequestProcessor<String>().process(producer, JMSEndpoint.DOMAIN_CHECK.toString(), new CheckDomainRequest(domainName, null, null));
+			return (String) new JMSRequestProcessor().process(producer, JMSEndpoint.DOMAIN_CHECK.toString(), new JMSReqRes(new CheckDomainRequest(domainName, null, null), null));
 		}catch(JMSResquestException e) {
 			logger.error(e.getMessage(), e);
 			throw new BaseException(e);
